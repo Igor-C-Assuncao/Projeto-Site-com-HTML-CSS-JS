9b8c1188e79bf29ac5a9c622d4f406d4466abd66
IGOR CASSIMIRO ASSUNCAO <igor.00000829605@unicap.br>
2023-10-26 21:16:24 -0300

    correçoes login

---
+++ login/SingUp.js
@@ -0,0 +1,33 @@
+Parse.initialize(
+    'qKODgViKtZxHFQhZuLtc6DZS43VmFcDvQsdvKFIQ','HF97YilffWsitI9bCjP78YcajJnkTSpu8bQa1IkL');
+  Parse.serverURL = "https://parseapi.back4app.com/";
+
+  document.getElementById("cadastroButton").addEventListener("click", async () => {
+    const myNewObject = new Parse.Object('Login');
+    myNewObject.set('email', document.getElementById("email").value);
+    myNewObject.set('password', document.getElementById("password").value);
+    myNewObject.set('Nome', document.getElementById("nome").value);
+    try {
+      const result = await myNewObject.save();
+      // Access the Parse Object attributes using the .GET method
+      console.log('Login created', result);
+    } catch (error) {
+      console.error('Error while creating Login: ', error);
+    }
+  });
+
+  const passwordInput = document.getElementById('password');
+  const eyeIcon = document.getElementById('eye');
+  let isShowingPassword = false;
+  
+  eyeIcon.addEventListener('mousedown', () => {
+    passwordInput.type = 'text';
+    isShowingPassword = true;
+  });
+  
+  document.addEventListener('mouseup', () => {
+    if (isShowingPassword) {
+      passwordInput.type = 'password';
+      isShowingPassword = false;
+    }
+  });
--- login/script.js
+++ login/script.js
@@ -51,16 +51,3 @@ document.addEventListener('mouseup', () => {
 
 
 
-document.getElementById("cadastroButton").addEventListener("click", async () => {
-  const myNewObject = new Parse.Object('Login');
-  myNewObject.set('email', document.getElementById("email").value);
-  myNewObject.set('password', document.getElementById("password").value);
-  myNewObject.set('Nome', document.getElementById("nome").value);
-  try {
-    const result = await myNewObject.save();
-    // Access the Parse Object attributes using the .GET method
-    console.log('Login created', result);
-  } catch (error) {
-    console.error('Error while creating Login: ', error);
-  }
-});
