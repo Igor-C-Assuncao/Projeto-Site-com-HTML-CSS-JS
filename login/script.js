0716cc0f2e389b91a3fc4b82b133d55d8d0ef889
IGOR CASSIMIRO ASSUNCAO <igor.00000829605@unicap.br>
2023-10-26 23:14:07 -0300

    alerta de login errado

--- login/script.js
+++ login/script.js
@@ -7,23 +7,26 @@ document.getElementById("loginButton").addEventListener("click", async () => {
     const password = document.getElementById("password").value;
     const login = Parse.Object.extend('Login');
     const query = new Parse.Query(login);
+    let loginSuccessful = false; // Variável para rastrear o sucesso do login
     
     try {
-
-    const login = await query.find();
+        const loginData = await query.find();
    
-
-    for( const object of login){
-        const getPass = object.get('password')
-        const getmail = object.get('email')
+        for (const object of loginData) {
+            const getPass = object.get('password');
+            const getmail = object.get('email');
       
-      if(getPass == password && getmail == email ){
-        alert("Login efetuado com sucesso. Bem-vindo, " + object.get("Nome"));
-    }
-    
-    
-    }
-   
+            if (getPass == password && getmail == email) {
+                alert("Login efetuado com sucesso. Bem-vindo, " + object.get("Nome"));
+                loginSuccessful = true; // Define a variável como true para indicar um login bem-sucedido
+                break; // Saia do loop, pois o login foi bem-sucedido
+            }
+        }
+        
+        // Verifique se o login não foi bem-sucedido e exiba uma mensagem de alerta
+        if (!loginSuccessful) {
+            alert("Usuário ou senha incorretos. Por favor, tente novamente.");
+        }
   
     
 
